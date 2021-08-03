f8b09b0910f6971668c53d867417e5e3fe431956
Jason <candlebirdgames@gmail.com>
2021-08-03 13:43:32 -0700

    Getting POSTs working

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require("firebase-functions");
+const cors = require("cors")({origin: true});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,4 +7,17 @@ const functions = require("firebase-functions");
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
+});
+
+exports.logFPS = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+    if (req.method !== "POST") {
+      return res.status(500).json({
+        message: "Not Allowed.",
+      });
+    }
+    res.status(200).json({
+      message: "Success: ",
+    });
+  });
 });
